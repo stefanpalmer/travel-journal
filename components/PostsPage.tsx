ef03ffbe22266d6de608a92592b300e0cccf2286
Stefan Palmer <palmerstefan@yahoo.com>
2023-03-03 11:11:48 +0100

    Display images and improve styling

--- components/PostsPage.tsx
+++ components/PostsPage.tsx
@@ -1,4 +1,5 @@
-import {Post} from '../typings'
+import {Post} from '../typings';
+import urlFor from "../lib/urlFor";
 
 type Props = {
     posts: Post[];
@@ -6,10 +7,17 @@ type Props = {
 
 function PostsPage({ posts }: Props) {
     return (
-        <div>
+        <div className="body-contain">
             {posts.map((post) => (
-                <div key={post._id}>
-                    <h4>{post.title}</h4>
+                <div key={post._id} className="entry-contain">
+                    <img src={urlFor(post.mainImage).url()} alt="" />
+                    <div className="entry-text">
+                        <h4>{post.title}</h4>
+                        <div>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe quidem commodi rem. Magnam similique delectus, vero reiciendis assumenda molestias doloribus vitae amet corporis, deleniti hic quia sit at quisquam repudiandae.
+                        </div>
+                        <a href="#">Read Post</a>
+                    </div>
                 </div>
             ))}
         </div>
